39b14f97137c9ad8db606fbde5593d851ebd23d2
estidlore <estidlore@outlook.com>
2022-10-15 23:01:18 -0500

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# NTT Audit
+---
+Engineering bot's solution for the 9th Sustainable Development Goal.
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import React from "react";
+
+const App: FC = (): JSX.Element => (
+  <div>{"NTT Audit"}</div>
+);
+
+export { App };
